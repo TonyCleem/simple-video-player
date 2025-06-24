2f5b8d840c309f42cec21af050360107a5906258
Tony Cleem <tonycleem@gmail.com>
2025-06-25 01:35:39 +0700

    fix livereload_server.py && add normalize.css

--- livereload_server.py
+++ livereload_server.py
@@ -2,5 +2,9 @@ from livereload import Server, shell
 
 
 server = Server()
-server.watch('.', shell('make html', cwd='.'))
+server.watch(
+    '*',
+     shell('make html', cwd='simple_video_player')
+     )
+     
 server.serve(root='.')
